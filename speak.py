e0cccd63c8fac6ef328b9902ff2d2baedf45ad4f
Thahirab <86869212+Thahirab@users.noreply.github.com>
2023-09-06 15:37:01 +0530

    Delete speak.py

--- speak.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-def talk(text):
-    engine = pyttsx3.init("sapi5")
-    voices = engine.getProperty('voices')
-    engine.setProperty('voices',voices[0].id)
-    engine.setProperty('rate',170)
-    print(f"A.I : {text}")
-    engine.say(text=text)
-    engine.runAndWait()
